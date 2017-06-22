faf8b26e0112983b414a609f75cd6b10c9fb9815
Вадим Стецюк < >
2017-06-22 13:10:26 +0300

    Add method for async getting companies from server by technology REST

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -16,10 +16,68 @@ export class CompanyService {
     getCompanies(): Observable<Company[]> {
         // let url = 'http://localhost:3000/api/companies';
         let url = 'assets/companies.json';
-        
+
+
+        return this.http.get(url)
+            .map((res: Response) => res.json())
+            .catch(this.handleError);
+    }
+
+    getCompanyById(_id: Number): Observable<Company> {
+        // let url = 'http://localhost:3000/api/companies/' + _id;
+        let url = 'assets/companies.json';
 
         return this.http.get(url)
             .map((res: Response) => res.json())
             .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    addCompany(_company: Company): Observable<Company> {
+        let body = JSON.stringify(_company);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        let url = 'http://localhost:3000/api/companies';
+
+        return this.http.post(url, body, options)
+            .map(res => <Company>res.json())
+            .catch(this.handleError);
+    }
+
+    editCompany(_company: Company): Observable<Company> {
+        let body = JSON.stringify(_company);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        let url = 'http://localhost:3000/api/companies/' + _company.id;
+
+        return this.http.put(url, body, options)
+            .map(res => <Company>res.json())
+            .catch(this.handleError);
+    }
+
+    deleteCompany(_id: Number): Observable<Company> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        let url = 'http://localhost:3000/api/companies/' + _id;
+
+        return this.http.delete(url, options)
+            .map(res => <Company>res.json())
+            .catch(this.handleError);
+    }
+
+    private handleError(err: any): Observable<string> {
+        let message: string;
+        if (err.message) {
+            message = err.message;
+        } else {
+            message = (err.status) ?
+                `${err.status} : ${err.statusText}` :
+                'Server connection error';
+        }
+
+        console.error(message);
+        return Observable.throw(message);
     }
 }
